45c1af9c8ebc0be1647007ff76caed12639e2008
soumyadip007 <soumyadip.note@gmail.com>
2019-04-03 00:18:53 +0530

    Test places Removed

--- app/src/main/java/com/example/myapplication/mapinit.java
+++ app/src/main/java/com/example/myapplication/mapinit.java
@@ -1,13 +1,18 @@
 package com.example.myapplication;
 
 import android.content.ContentValues;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 import org.osmdroid.DefaultResourceProxyImpl;
 import org.osmdroid.ResourceProxy;
 import org.osmdroid.api.IMapController;
-import org.osmdroid.tileprovider.MapTileProviderBasic;
-import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.tileprovider.tilesource.XYTileSource;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
@@ -16,43 +21,8 @@ import org.osmdroid.views.overlay.MyLocationOverlay;
 import org.osmdroid.views.overlay.OverlayItem;
 import org.osmdroid.views.overlay.ScaleBarOverlay;
 import org.osmdroid.views.overlay.SimpleLocationOverlay;
-import org.osmdroid.views.overlay.TilesOverlay;
-
-import android.database.sqlite.SQLiteDatabase;
-import android.location.Location;
-
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.graphics.drawable.Drawable;
-import android.location.Location;
-import android.location.LocationManager;
-import android.os.Bundle;
-import android.view.ViewGroup;
 
 import java.util.ArrayList;
-import android.content.Context;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.Build;
-import android.os.Bundle;
-import android.provider.Settings;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-
-import android.app.Activity;
-import android.content.Context;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.graphics.drawable.Drawable;
-import android.location.Location;
-import android.location.LocationManager;
-import android.os.Bundle;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 public class mapinit extends AppCompatActivity {
 
@@ -116,13 +86,12 @@ public class mapinit extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapinit);
-        map = (MapView) findViewById(R.id.map);
+        map = findViewById(R.id.map);
 
-        map.setTileSource(new XYTileSource("OSMPublicTransport",ResourceProxy.string.public_transport, 0, 16, 256, ".png", new String[] {
-                "http://otile1.mqcdn.com/tiles/1.0.0/map/",
+        map.setTileSource(new XYTileSource("OSMPublicTransport", ResourceProxy.string.public_transport, 0, 16, 256, ".png", "http://otile1.mqcdn.com/tiles/1.0.0/map/",
                 "http://otile2.mqcdn.com/tiles/1.0.0/map/",
                 "http://otile3.mqcdn.com/tiles/1.0.0/map/",
-                "http://otile4.mqcdn.com/tiles/1.0.0/map/"}));
+                "http://otile4.mqcdn.com/tiles/1.0.0/map/"));
         map.setBuiltInZoomControls(true);
         map.setMultiTouchControls(true);
         map.setUseDataConnection(false);
@@ -148,22 +117,22 @@ public class mapinit extends AppCompatActivity {
         this.mScaleBarOverlay = new ScaleBarOverlay(this);
         map.getOverlays().add(mScaleBarOverlay);
         resourceProxy = new DefaultResourceProxyImpl(getApplicationContext());
-        GeoPoint  currentLocation = new GeoPoint(22.5567,88.3022);
-        GeoPoint  currentLocation2 = new GeoPoint(22.513365,88.403003);
-        OverlayItem myLocationOverlayItem = new OverlayItem("Here", "Current Position", currentLocation);
-        Drawable myCurrentLocationMarker = this.getResources().getDrawable(R.drawable.ic_launcher);
-        myLocationOverlayItem.setMarker(myCurrentLocationMarker);
-
-
+//        GeoPoint  currentLocation = new GeoPoint(22.5567,88.3022);
+//        GeoPoint  currentLocation2 = new GeoPoint(22.513365,88.403003);
+//        OverlayItem myLocationOverlayItem = new OverlayItem("Here", "Current Position", currentLocation);
+//        Drawable myCurrentLocationMarker = this.getResources().getDrawable(R.drawable.ic_launcher);
+//        myLocationOverlayItem.setMarker(myCurrentLocationMarker);
+//
+//
         ArrayList<OverlayItem> items = upload();
         if(items==null)
             items=new ArrayList<OverlayItem>();
 
-        items.add(myLocationOverlayItem);
-        myLocationOverlayItem = new OverlayItem("Here", "Current Position", currentLocation2);
-        myCurrentLocationMarker = this.getResources().getDrawable(R.drawable.ic_launcher);
-        myLocationOverlayItem.setMarker(myCurrentLocationMarker);
-        items.add(myLocationOverlayItem);
+//        items.add(myLocationOverlayItem);
+//        myLocationOverlayItem = new OverlayItem("Here", "Current Position", currentLocation2);
+//        myCurrentLocationMarker = this.getResources().getDrawable(R.drawable.ic_launcher);
+//        myLocationOverlayItem.setMarker(myCurrentLocationMarker);
+//        items.add(myLocationOverlayItem);
 
 
         currentLocationOverlay = new ItemizedIconOverlay<OverlayItem>(items,
