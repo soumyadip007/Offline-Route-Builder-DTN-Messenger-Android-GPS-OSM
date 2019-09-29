2c7a0592d85fcf946c9520920fd51837dbff2ec4
soumyadip007 <soumyadip.note@gmail.com>
2019-09-29 21:53:43 +0530

    Test Removed

--- app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.java
@@ -1,20 +1,10 @@
 package com.example.myapplication;
 
-import android.content.Context;
-
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.platform.app.InstrumentationRegistry;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import static org.junit.Assert.assertEquals;
-
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
@@ -25,3 +15,4 @@ public class ExampleInstrumentedTest {
         assertEquals("com.example.myapplication", appContext.getPackageName());
     }
 }
+ */
